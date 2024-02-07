dc7f6c4da2a359031e6c0862c1b457201313a047
Tiziozero <kleidiv123@gmail.com>
2024-02-07 21:33:45 +0000

    fixed client mess

--- src/client.py
+++ src/client.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-from typing_extensions import KeysView
 import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,29 +8,31 @@ sock.connect(("139.162.200.195", 8080))
 ON = True
 
 def recv_method(sock):
-    while ON:
+    while True:
         try:
             data = sock.recv(1024).decode()
             print(data)
         except BrokenPipeError as e:
             print(f"Error {e}")
-            ON = False
-        except:
-            pass
+            break
+        except Exception as e:
+            print("Error:", e)
+            break
 
 def send_method(sock):
-    while ON:
+    while True:
         try:
             data = input(">").encode()
-            sock.send_all(data)
+            sock.send(data)
         except BrokenPipeError as e:
             print(f"Error {e}")
             ON = False
         except KeyboardInterrupt:
             print("KI. Disconnecting.")
-            ON = False
-        except:
-            pass
+            break
+        except Exception as e:
+            print("Error:", e)
+            break
 
 recv_thread = threading.Thread(target=recv_method, args=(sock,))
 send_thread = threading.Thread(target=send_method, args=(sock,))
