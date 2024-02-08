6e6152417ab21b56d2a3a813b0b0676a9317d68f
Tiziozero <kleidiv123@gmail.com>
2024-02-08 20:07:57 +0000

    fixing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::net::{Shutdown, TcpListener, TcpStream};
-use std::{io, thread};
-use std::sync::{mpsc, Arc, Mutex};
-use uuid::Uuid;
-use std::io::{Read, Write};
-
 use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::sync::{mpsc, Arc, Mutex};
+use std::{io, thread};
+use uuid::Uuid;
 
 #[derive(Debug)]
 struct Message {
     name: String,
     content: String,
+    client_token: String,
     message_id: String,
 }
 
@@ -22,58 +22,64 @@ struct Client {
 
 impl Client {
     fn clone(&self) -> Result<Self, String> {
-        Ok(
-            Client {
-                name: self.name.clone(),
-                token: self.token.clone(),
-                stream: self.stream.try_clone().unwrap()
-            }
-        )
+        Ok(Client {
+            name: self.name.clone(),
+            token: self.token.clone(),
+            stream: self.stream.try_clone().unwrap(),
+        })
     }
 }
 
-
-fn handle_client(tx: mpsc::Sender<Message>,mut client: Client) -> std::io::Result<()> {
+fn handle_client(tx: mpsc::Sender<Message>, mut client: Client) -> std::io::Result<()> {
     loop {
         let mut buf = [0u8; 1024];
         let amt = match client.stream.read(&mut buf) {
             Ok(0) => {
-                eprintln!("Received 0 from {}. Disconnecting.", client.stream.peer_addr()?);
+                eprintln!(
+                    "Received 0 from {}. Disconnecting.",
+                    client.stream.peer_addr()?
+                );
                 client.stream.shutdown(Shutdown::Both)?;
-                return Err(std::io::Error::new(std::io::ErrorKind::Other, "Receivedd 0 from client"));
-            },
-            Ok(n) => { n },
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Receivedd 0 from client",
+                ));
+            }
+            Ok(n) => n,
             Err(_) => {
-                eprintln!("Error for {}. Disconnecting.",
-                    client.stream.peer_addr()?);
+                eprintln!("Error for {}. Disconnecting.", client.stream.peer_addr()?);
                 client.stream.shutdown(Shutdown::Both)?;
-                return Err(
-                    std::io::Error::new(
-                        std::io::ErrorKind::Other,
-                        "Unexpected error"
-                        )
-                    );
-            },
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Unexpected error",
+                ));
+            }
         };
         if let Ok(txt) = std::str::from_utf8(&buf[..amt]) {
-            let msg = Message{
+            let msg = Message {
                 name: client.name.clone(),
                 content: txt.to_string(),
-                message_id: Uuid::new_v4().to_string()
+                client_token: client.token.clone(),
+                message_id: Uuid::new_v4().to_string(),
             };
             match tx.send(msg) {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(_) => {
-                    eprintln!("Received 0 from {}. Disconnecting.", client.stream.peer_addr()?);
-                    return Err(std::io::Error::new(std::io::ErrorKind::Other, "Error sending to tx"));
-                },
+                    eprintln!(
+                        "Received 0 from {}. Disconnecting.",
+                        client.stream.peer_addr()?
+                    );
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Error sending to tx",
+                    ));
+                }
             }
         } else {
             eprintln!("Error in decoding message");
         };
     }
 }
-
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -82,56 +88,42 @@ fn main() -> std::io::Result<()> {
     let mut clients = Arc::new(Mutex::new(HashMap::<String, Client>::new()));
     let clients_arc = Arc::clone(&clients);
 
-    let _ = thread::spawn( move || {
-        loop {
-            println!("Receiving in rx");
-            let msg = rx.recv().unwrap();
-            println!("{:?}", msg);
-            let m = format!("[ {} ]::< {} >", msg.name, msg.content);
-            let message = m.into_bytes();
+    let _ = thread::spawn(move || loop {
+        println!("Receiving in rx");
+        let msg = rx.recv().unwrap();
+        println!("{:?}", msg);
+        let m = format!("[ {} ]::< {} >", msg.name, msg.content);
+        let message = m.into_bytes();
 
-            let mut clients = clients_arc.lock().unwrap();
-            let mut remove_v: Vec<String> = Vec::new();
-            /*for (_ , mut c) in v.iter_mut() {
-                    match c.stream.write_all(&message) {
-                            Ok(_) => {
-                                    println!("wrote to client {}", c.stream.local_addr().unwrap());
-                            },
-                            Err(e) => {
-                                    if e.kind() == io::ErrorKind::BrokenPipe {
-                                            eprintln!("Broken PipeLine Error from {}. Disconnecting.", c.stream.local_addr().unwrap());
-                                    }
-                                    remove_v.push(c.token.clone());
-                            },
-                    }
-            }
-            for c in v.iter() {
-                    let _ = v.remove(&c);
-                    println!("Removed client with token {}", &c);
-                                    
-            }*/
-            for (token, client) in clients.iter_mut() {
+        let mut clients = clients_arc.lock().unwrap();
+        let mut remove_v: Vec<String> = Vec::new();
+
+        for (token, client) in clients.iter_mut() {
+            if msg.client_token != client.token {
                 match client.stream.write_all(&message) {
                     Ok(_) => println!("Wrote to client {}", client.stream.local_addr().unwrap()),
-
                     Err(e) if e.kind() == io::ErrorKind::BrokenPipe => {
-                        eprintln!("Broken Pipe Error from {}. Disconnecting.", client.stream.local_addr().unwrap());
+                        eprintln!(
+                            "Broken Pipe Error from {}. Disconnecting.",
+                            client.stream.local_addr().unwrap()
+                        );
                         remove_v.push(token.clone());
-                    },
-
-                    Err(e) => eprintln!("Error writing to client {}: {}", client.stream.local_addr().unwrap(), e),
-                }
-            }
-
-            for token in remove_v {
-                if clients.remove(&token).is_some() {
-                    println!("Removed client with token {}", token);
-
+                    }
+                    Err(e) => eprintln!(
+                        "Error writing to client {}: {}",
+                        client.stream.local_addr().unwrap(),
+                        e
+                    ),
                 }
             }
         }
 
-    } );
+        for token in remove_v {
+            if clients.remove(&token).is_some() {
+                println!("Removed client with token {}", token);
+            }
+        }
+    });
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
@@ -143,16 +135,18 @@ fn main() -> std::io::Result<()> {
                     stream: s,
                 };
 
-                clients.lock().unwrap().insert(c.token.clone(), c.clone().unwrap());
+                clients
+                    .lock()
+                    .unwrap()
+                    .insert(c.token.clone(), c.clone().unwrap());
 
-                let _ = thread::spawn( move || {
+                let _ = thread::spawn(move || {
                     let _ = handle_client(tx, c.clone().unwrap());
                 });
-            },
-            Err(_) => {},
+            }
+            Err(_) => {}
         }
     }
 
     Ok(())
 }
-
